87b9ce88f19a7201553c832690b77b9509880b8b
faresakkad <151541669+faresakkad@users.noreply.github.com>
2023-11-27 19:12:54 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,49 @@
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QPushButton
+from PyQt5.QtGui import QPainter, QColor, QPolygon
 import sys
-from ui_file import Ui_MainWindow
-from random import randint
-from PyQt5.QtGui import QColor, QPainter
-from PyQt5.QtWidgets import QApplication
-from PyQt5.QtWidgets import QMainWindow
+import random
+from UI import Ui_MainWindow
+
+SCREEN_SIZE = [680, 480]
 
 
-class MyWidget(QMainWindow, Ui_MainWindow):
+class Example(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.do_paint = False
-        self.pushButton.clicked.connect(self.create)
+        self.flag = False
+        self.setWindowTitle('Супрематизм')
+        self.pushButton.clicked.connect(self.draw)
+        self.coords = []
+
+    def draw(self):
+        self.figure = 'circle'
+        self.size = random.randint(10, 100)
+        self.color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))  # 'yellow'
+        self.flag = True
+        self.update()
 
     def paintEvent(self, event):
-        if self.do_paint:
+        if self.flag:
             qp = QPainter()
             qp.begin(self)
-            self.draw(qp)
+            qp.setPen(QColor(*self.color))
+            qp.setBrush(QColor(*self.color))
+            self.x, self.y = random.randint(100, SCREEN_SIZE[0] - 100), random.randint(100, SCREEN_SIZE[1] - 100)
+            if self.figure == 'circle':
+                qp.drawEllipse(self.x, self.y, self.size, self.size)
             qp.end()
 
-    def draw(self, qp):
-        qp.setBrush(QColor(randint(0, 255), randint(0, 255), randint(0, 255)))
-        r = randint(0, 250)
-        qp.drawEllipse(randint(0, 399 - r), randint(0, 561 - r), r, r)
-        self.do_paint = False
 
-    def create(self):
-        self.do_paint = True
-        self.repaint()
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = MyWidget()
+    sys.excepthook = except_hook
+    ex = Example()
     ex.show()
-    sys.exit(app.exec())
+    sys.exit(app.exec_())
